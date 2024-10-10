42327a10b436de327d1660855b57d16e5dabbac1
g.sawada <sawada.gota@gmail.com>
2024-10-10 23:49:43 +0900

    layout.jsx編集

--- public/src/app/layout.jsx
+++ public/src/app/layout.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-
-const layout = () => {
-  return <div>layout</div>;
-};
-
-export default layout;
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
