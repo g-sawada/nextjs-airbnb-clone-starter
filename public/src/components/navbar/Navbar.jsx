17bded80f29a31ff77a2490c0f233f453ec4e6fc
g.sawada <sawada.gota@gmail.com>
2024-10-11 23:23:51 +0900

    Navbar.jsxの編集

--- public/src/components/navbar/Navbar.jsx
+++ public/src/components/navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import AirbnbLogo from "../../svg/airbnb-logo";
+
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full flex flex-col justify-center transition-all duration-3">
+      <div className="flex items-center justify-between px-20">
+        <div className="flex-grow basis-0">
+          <div className="w-max cursor-pointer">
+            < AirbnbLogo />
+          </div>
+        </div> 
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
