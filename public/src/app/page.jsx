26aafb676a7704989f4906b21bd32e2d907a195c
g.sawada <sawada.gota@gmail.com>
2024-10-12 19:03:55 +0900

    pageにAuthmodalコンポーネントを追加

--- public/src/app/page.jsx
+++ public/src/app/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "airbnb/components/footer/Footer";
 import Navbar from "airbnb/components/navbar/Navbar";
+import AuthModal from "airbnb/components/auth/AuthModal";
 
 const page = () => {
   return <div>
     <Navbar/>
     <Footer/>
+    <AuthModal/>
   </div>;
 };
 
