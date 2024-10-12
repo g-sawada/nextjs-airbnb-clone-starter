dc07fbafa4c8ce30faebe77e0dcd47db8ba91e4a
g.sawada <sawada.gota@gmail.com>
2024-10-12 19:35:30 +0900

    フォントデータをlayoutに追加（動作してない？）

--- public/src/app/layout.jsx
+++ public/src/app/layout.jsx
@@ -1,9 +1,16 @@
-import "./globals.css"
+import "./globals.css";
+import {Inter} from "next/font/google";
+
+const inter = Inter({subsets:["latin"]});
+
+export const metadata = {
+  title: "Airbnb Clone",
+}
 
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
